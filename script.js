de787af52f93e429afa3f6b5f34300d81bfd7124
Venkateshkumar K <115687050+Venkateshkumar1@users.noreply.github.com>
2022-11-20 14:23:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let myResume={
+    "basics": {
+      "name": "VENKATESHKUMAR.K",
+      "email": "kishorkani4321@gamil.com",
+      "phone": 9500873866,
+      "degree": "B.E",
+      "department":"Mechanical Engineering",
+      "location": {
+        "address": "244,Anthiyur",
+        "postalCode": 638314,
+        "city": "Erode",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Venkateshkumar1",
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "K.S.Rangasamy College Of Technology",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2021,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "HTML,CSS,javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,coading pratice",
+      }
+    ]
+  }
+  console.log(myResume);
+  
